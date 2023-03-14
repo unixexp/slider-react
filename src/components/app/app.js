3b5c6bbde98aeadd8a3006a16c9bc1f0f6fb07e3
Yurii Rohoza <unixexp@gmail.com>
2023-03-14 18:16:09 +0200

    init repo

---
+++ src/components/app/app.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+
+const App = () => {
+
+    return (
+        <div>
+            Welcome to empty react application!
+        </div>
+    );
+
+};
+
+export default App;
